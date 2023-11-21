dea55e706973b9c01a97fc6e8d7c3548d4bf71c2
Peyton Meredith <peymeredith@gmail.com>
2023-11-20 20:03:16 -0600

    finish library practice

--- server/app.py
+++ server/app.py
@@ -15,13 +15,63 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 db.init_app(app)
+api = Api(app)
 
 @app.route('/')
 def index():
     return '<h1>Library Practice Challenge</h1>'
+
+
+class Books(Resource):
+    def get(self):
+        books = [b.to_dict(only=('id','author','title')) for b in Book.query.all()]
+        return make_response(
+            books,
+            200
+        )
+api.add_resource(Books, '/books')
+
+class BookByID(Resource):
+    def get(self, id):
+        book = Book.query.get( id )
+        if not book:
+            return make_response({ 'error': 'Book not found'}, 404)
+        return make_response(book.to_dict(), 200)
+    
+    def delete(self, id):
+        book = Book.query.get( id )
+        if not book:
+            return make_response({'error': 'Book not found'}, 404)
+        db.session.delete(book)
+        db.session.commit()
+        return make_response('', 204)
+        
+api.add_resource(BookByID, '/books/<int:id>')
+
+
+class Members(Resource):
+    def get(self):
+        members = [m.to_dict(only=('id', 'name', 'year_joined')) for m in Member.query.all()]
+        return make_response(
+            members,
+            200
+        )
+api.add_resource(Members, '/members')
+
+class Loans(Resource):
+    def post(self):
+        params = request.json
+        try:
+            loan = Loan(book_id=params['book_id'], member_id=params['member_id'])
+        except ValueError as v_error:
+            return make_response({'error': [str(v_error)]}, 422)
+        db.session.add(loan)
+        db.session.commit()
+        return make_response(loan.to_dict(), 201)
+api.add_resource(Loans, '/loans')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql.expression import bindparam
 from sqlalchemy_serializer import SerializerMixin
 from datetime import timedelta, datetime
 
+
 def next_week():
     return datetime.utcnow() + timedelta(days=7)
 
@@ -14,6 +15,7 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+
 
 class Book(db.Model, SerializerMixin):
     __tablename__ = 'books'
@@ -24,8 +26,16 @@ class Book(db.Model, SerializerMixin):
     number_of_pages = db.Column(db.Integer)
 
     # add relationship
-
+    loans = db.relationship('Loan', back_populates='book', cascade='all, delete-orphan')
+    members = association_proxy('loans', 'member')
     # add serialization rules
+    serialize_rules = ('-loans.book',)
+
+    @validates('number_of_pages')
+    def validates_pages(self, key, page_num):
+        if page_num < 1:
+            raise ValueError("Book must have at least 1 page")
+        return page_num
 
     def __repr__(self):
         return f'<Book {self.id} {self.title} {self.author} {self.number_or_pages}>'
@@ -39,8 +49,10 @@ class Member(db.Model, SerializerMixin):
     year_joined = db.Column(db.String)
 
     # add relationship
-    
+    loans = db.relationship('Loan', back_populates='member', cascade='all, delete-orphan')
+    books= association_proxy('loans', 'book')
     # add serialization rules
+    serialize_rules = ('-loans.member',)
 
     def __repr__(self):
         return f'<Member {self.id} {self.name} {self.year_joined}>'
@@ -52,10 +64,15 @@ class Loan(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     check_out_date = db.Column(db.DateTime, server_default=db.func.now())
     due_date = db.Column(db.DateTime, default=next_week)
+    member_id = db.Column(db.Integer, db.ForeignKey('members.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
 
     # add relationship
+    member = db.relationship('Member', back_populates='loans')
+    book = db.relationship('Book', back_populates='loans')
 
     # add serialization rules
+    serialize_rules = ('-book.loans', '-member.loans',)
 
     def __repr__(self):
         return f'<Loan {self.id} {self.check_out_date} {self.due_date}>'
